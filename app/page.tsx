98157046117712049b8e85879999c4f97e96955c
Nathan Glass <85080878+Visibyte@users.noreply.github.com>
2023-11-21 20:19:56 +0000

    start feature list

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
 import { Button } from "@/components/ui/button";
 import MaxWidthWrapper from "@/components/ui/maxWidthWrapper";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Download } from "lucide-react";
 import Link from "next/link";
+
+const features = [
+  {
+    label: "Instant Access",
+    icon: Download,
+    description: "Instantly download your assets"
+  },
+  {
+    label: "Instant Access 2",
+    icon: Download,
+    description: "Instantly download your assets"
+  },
+  {
+    label: "Instant Access 3",
+    icon: Download,
+    description: "Instantly download your assets"
+  }
+]
 
 const Home = () => {
   return (
@@ -26,9 +44,25 @@ const Home = () => {
           </div>
         </div>
         {/* TODO: feature grid */}
-        <div>
-
-        </div>
+        <section className="bg-gray-50 rounded-lg ">
+          <MaxWidthWrapper className="py-20">
+            <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
+              {features.map((feature, index) => (
+                <div key={index} className="text-center md:flex md:items-center md:text-left lg:block lg:text-center">
+                  <div className="md:flex-shrink-0 flex justify-center">
+                    <div className="h-16 w-16 flex items-center justify-center rounded-full bg-green-100 text-green-900">
+                      {<feature.icon className="w-1/3 h-1/3" />}
+                    </div>
+                  </div>
+                  <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
+                    <h2 className="text-lg font-medium text-gray-900">{feature.label}</h2>
+                    <p className="mt-3 text-">{feature.description}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </MaxWidthWrapper>
+        </section>
       </main>
     </MaxWidthWrapper>
   )
