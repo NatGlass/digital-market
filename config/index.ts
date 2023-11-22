249fe4239953b60815de309f2c568e723431cab6
Visibyte <nathanglass26@gmail.com>
2023-11-22 19:40:17 +0000

    nav categories

---
+++ config/index.ts
@@ -0,0 +1,22 @@
+export const CATEGORIES = [
+  {
+    label: "UI Kits",
+    value: "ui_kits" as const,
+    featured: [
+      {
+        name: "Editor's Picks",
+        href: "#",
+      },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    featured: [
+      {
+        name: "Editor's Picks",
+        href: "#",
+      },
+    ],
+  },
+];
