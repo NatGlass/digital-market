ba1de9b597027faf9552e561d315c372eb534aaa
Visibyte <nathanglass26@gmail.com>
2023-11-22 19:40:12 +0000

    nav items

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,7 +1,38 @@
-"use client"
+"use client";
+
+import { useState } from "react";
+import { CATEGORIES } from "@/config";
+import NavItem from "./NavItem";
 
 const NavItems = () => {
-  return <div>NavItems</div>;
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  const isAnyOpen = activeIndex !== null;
+  return (
+    <div className="flex gap-4 h-full">
+      {CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        };
+
+        const isOpen = i === activeIndex;
+
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default NavItems;
